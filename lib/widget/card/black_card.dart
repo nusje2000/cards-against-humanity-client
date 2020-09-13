40bb0135bb27c5cc800a2d1c9255e3d11c7ae4e5
ArchBacon <c.delicaat@hotmail.com>
2020-09-13 19:29:33 +0200

    Created black and white cards

---
+++ lib/widget/card/black_card.dart
@@ -0,0 +1,6 @@
+import 'package:cards_against_humanity/widget/card/card.dart';
+import 'package:flutter/material.dart';
+
+class BlackCard extends BaseCard {
+  BlackCard({text}) : super(color: Colors.black, text: text);
+}
