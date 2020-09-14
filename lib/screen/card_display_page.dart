00cdbf58a793cdb81ab648446369c8591bf997fc
ArchBacon <c.delicaat@hotmail.com>
2020-09-14 02:56:59 +0200

    Added field for background color

--- lib/screen/card_display_page.dart
+++ lib/screen/card_display_page.dart
@@ -3,6 +3,8 @@ import 'package:cards_against_humanity/widget/card/answer_card.dart';
 import 'package:flutter/material.dart';
 
 class CardDisplayPage extends StatefulWidget {
+  final Color backgroundColor = Color.fromRGBO(55, 71, 79, 1);
+
   @override
   _CardDisplayPageState createState() => _CardDisplayPageState();
 }
@@ -11,7 +13,7 @@ class _CardDisplayPageState extends State<CardDisplayPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(55, 71, 79, 1),
+      backgroundColor: widget.backgroundColor,
       body: SafeArea(
         child: Center(
           child: Column(
