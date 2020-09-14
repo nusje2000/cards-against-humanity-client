82be2d723b15013a2cce1d4bf912f4d5d5132e16
ArchBacon <c.delicaat@hotmail.com>
2020-09-14 02:05:39 +0200

    Added lobby grid builder

--- lib/screen/lobby_page.dart
+++ lib/screen/lobby_page.dart
@@ -1,7 +1,12 @@
+import 'package:cards_against_humanity/widget/lobby/lobby.dart';
 import 'package:flutter/material.dart';
 
 class LobbyPage extends StatefulWidget {
-  LobbyPage({Key key}) : super(key: key);
+  final List<Lobby> lobbies = [
+    Lobby(roomName: 'EdgeLord\'s room', roomId: 'AX6T'),
+    Lobby(roomName: 'Santa\'s room', roomId: '56D8'),
+    Lobby(roomName: 'Mom\'s room', roomId: 'D95F'),
+  ];
 
   @override
   _LobbyPageState createState() => _LobbyPageState();
@@ -11,9 +16,18 @@ class _LobbyPageState extends State<LobbyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(55, 71, 79, 1),
       body: SafeArea(
-        child: Center(
-          child: Text('Welcome to the lobby page'),
+        child: GridView.count(
+          scrollDirection: Axis.vertical,
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          padding: EdgeInsets.all(10),
+          children: List<Lobby>.generate(
+            widget.lobbies.length,
+            (index) => widget.lobbies[index],
+          ),
         ),
       ),
     );
