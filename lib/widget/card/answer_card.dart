484a1832985e11923d8a0e96657ec279e97688b9
ArchBacon <c.delicaat@hotmail.com>
2020-09-14 02:53:58 +0200

    Changed named optional parameters to optional parameters

--- lib/widget/card/answer_card.dart
+++ lib/widget/card/answer_card.dart
@@ -2,5 +2,5 @@ import 'package:cards_against_humanity/widget/card/card.dart';
 import 'package:flutter/material.dart';
 
 class AnswerCard extends BaseCard {
-  AnswerCard({text}) : super(color: Colors.white, text: text);
+  AnswerCard([text]) : super(color: Colors.white, text: text);
 }
--- lib/widget/card/question_card.dart
+++ lib/widget/card/question_card.dart
@@ -2,5 +2,5 @@ import 'package:cards_against_humanity/widget/card/card.dart';
 import 'package:flutter/material.dart';
 
 class QuestionCard extends BaseCard {
-  QuestionCard({text}) : super(color: Colors.black, text: text);
+  QuestionCard([text]) : super(color: Colors.black, text: text);
 }
