3f3553e90bccc26897b55ef23c4d235520bf1164
ArchBacon <c.delicaat@hotmail.com>
2020-09-14 02:53:00 +0200

    Renamed card classes

--- lib/widget/card/answer_card.dart
+++ lib/widget/card/answer_card.dart
@@ -1,6 +1,6 @@
 import 'package:cards_against_humanity/widget/card/card.dart';
 import 'package:flutter/material.dart';
 
-class WhiteCard extends BaseCard {
-  WhiteCard({text}) : super(color: Colors.white, text: text);
+class AnswerCard extends BaseCard {
+  AnswerCard({text}) : super(color: Colors.white, text: text);
 }
--- lib/widget/card/question_card.dart
+++ lib/widget/card/question_card.dart
@@ -1,6 +1,6 @@
 import 'package:cards_against_humanity/widget/card/card.dart';
 import 'package:flutter/material.dart';
 
-class BlackCard extends BaseCard {
-  BlackCard({text}) : super(color: Colors.black, text: text);
+class QuestionCard extends BaseCard {
+  QuestionCard({text}) : super(color: Colors.black, text: text);
 }
