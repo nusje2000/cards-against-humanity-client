c94e81d40d380643b107e1e17306b6eea4ba7917
ArchBacon <c.delicaat@hotmail.com>
2020-09-14 02:54:27 +0200

    Added page for seeing both answer and question cards at the same time

---
+++ lib/screen/card_display_page.dart
@@ -0,0 +1,30 @@
+import 'package:cards_against_humanity/widget/card/question_card.dart';
+import 'package:cards_against_humanity/widget/card/answer_card.dart';
+import 'package:flutter/material.dart';
+
+class CardDisplayPage extends StatefulWidget {
+  @override
+  _CardDisplayPageState createState() => _CardDisplayPageState();
+}
+
+class _CardDisplayPageState extends State<CardDisplayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(55, 71, 79, 1),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              QuestionCard('Example Text'),
+              SizedBox(height: 50),
+              AnswerCard('Example Text'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
