806967848d78e7bb39b5629e863e23fbdb763423
ArchBacon <c.delicaat@hotmail.com>
2020-09-20 16:18:39 +0200

    Added env file to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:cards_against_humanity/screen/rooms_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
 
-void main() {
+void main() async {
+  await FlutterConfig.loadEnvVariables();
+
   runApp(MaterialApp(
     title: 'Card Against Humanity',
     home: RoomsPage(),
